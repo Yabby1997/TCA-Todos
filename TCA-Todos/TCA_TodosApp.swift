0727bc150d3f9ec53db6ccfb2e5d53669ac8fa03
Yabby1997 <yabby1997@gmail.com>
2022-11-27 13:49:08 +0900

    Initial Commit

---
+++ TCA-Todos/TCA_TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TodosApp.swift
+//  TCA-Todos
+//
+//  Created by USER on 2022/11/27.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
