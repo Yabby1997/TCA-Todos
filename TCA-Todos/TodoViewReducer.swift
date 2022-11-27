eb0dd94f83cb6f78a2a4f95e4acc3a7049367d08
Yabby1997 <yabby1997@gmail.com>
2022-11-27 17:57:13 +0900

    TodoViewReducer 정의

--- TCA-Todos/TodoViewReducer.swift
+++ TCA-Todos/TodoViewReducer.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+struct TodoViewReducer: ReducerProtocol {
+    typealias State = Todo
+
+    enum Action: Equatable {
+        case priorityToggled
+        case doneToggled
+        case descriptionChanged(String)
+        case deadlineChanged(Date)
+    }
+
+    func reduce(into state: inout Todo, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .priorityToggled:
+            state.priority = state.priority.nextPriority()
+            return .none
+        case .doneToggled:
+            state.isDone.toggle()
+            return .none
+        case let .descriptionChanged(description):
+            state.description = description
+            return .none
+        case let .deadlineChanged(deadline):
+            state.deadline = deadline
+            return .none
+        }
+    }
+}
